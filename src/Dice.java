a04fc526d1c33dede05541bdf33c1e7219dfaf24
ochoa <ochoa@Max.attlocal.net>
2016-12-06 20:09:01 -0800

    Using super

---
+++ src/Dice.java
@@ -0,0 +1,19 @@
+
+public class Dice extends Racing {
+    
+    
+   
+    
+    public static int rollDice (int numberOfDice) {
+        
+        int LastRoll = 0; 
+        
+        
+   
+
+
+		return LastRoll += (1 + (int) ( Math.random() * 6 ));
+		
+	}
+
+}
